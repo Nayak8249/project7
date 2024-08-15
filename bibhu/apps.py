04769b7061c405deb37368e727942f7cd110d8b1
Nayak8249 <bibhuti824971@gmail.com>
2024-08-15 13:15:21 +0530

    first commit

---
+++ bibhu/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BibhuConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bibhu'
---
+++ bibhu/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def longboat(request):
+    return HttpResponse('this is good pub for drink')
+
